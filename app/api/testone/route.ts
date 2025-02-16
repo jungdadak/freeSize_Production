6c88cea06c0935c6fb6ad6a4aa0beeb9bdea04e2
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-02-16 21:26:36 +0900

    feat: EC2 Redis 배포완료, Jest 테스트코드 작성 및 연결체크

--- app/api/testone/route.ts
+++ app/api/testone/route.ts
@@ -10,6 +10,11 @@ const optionParamMap: Record<FileMethod, string> = {
   square: 'targetRes',
 };
 
+/**
+ * 클라이언트가 보낸 폼 데이터를 받아와서 spring swagger 스펙에 맞춰 요청을 보냅니다.
+ * 협업 편의성을 위해 zod 로 전송되는 데이터가 스키마 규격에 맞는지 재차검증합니다.
+ * spring response는 변경될 수 있으므로 별도 검증을 수행하지 않습니다.
+ */
 export async function POST(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
@@ -22,7 +27,7 @@ export async function POST(request: NextRequest) {
         { status: 400 }
       );
     }
-
+    //--------< zod schema로 spring에 데이터 전달전 최종 점검>--------------
     const validatedData = apiRequestSchema.parse({
       name: file.name,
       size: file.size,
@@ -61,6 +66,7 @@ export async function POST(request: NextRequest) {
     const result = await response.json();
     return NextResponse.json(result);
   } catch (error) {
+    //zod에러 발생은 클라이언트쪽에서 무언가 이상행동을 했을 가능성이 높아 400처리
     if (error instanceof z.ZodError) {
       return NextResponse.json(
         { error: error.errors[0].message },
